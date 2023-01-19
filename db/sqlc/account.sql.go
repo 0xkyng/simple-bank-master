b4421b3c03e7da8a349fa507c48972a5a4b194c0
Isaac Wanger <teryima127@gmail.com>
2023-01-19 10:26:14 +0100

    list accounts

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -68,11 +68,20 @@ func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
 
 const listAccounts = `-- name: ListAccounts :many
 SELECT id, owner, currency, balance, created_at FROM accounts
+WHERE owner = $1
 ORDER BY id
+LIMIT $2
+OFFSET $3
 `
 
-func (q *Queries) ListAccounts(ctx context.Context) ([]Account, error) {
-	rows, err := q.db.QueryContext(ctx, listAccounts)
+type ListAccountsParams struct {
+	Owner  string `json:"owner"`
+	Limit  int32  `json:"limit"`
+	Offset int32  `json:"offset"`
+}
+
+func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error) {
+	rows, err := q.db.QueryContext(ctx, listAccounts, arg.Owner, arg.Limit, arg.Offset)
 	if err != nil {
 		return nil, err
 	}
